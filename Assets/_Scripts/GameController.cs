f1279b8c3b9c560b74d96dd1d126511d579c9fb7
José Juan Sánchez <josejuan.sanchez@gmail.com>
2016-06-06 23:34:13 +0200

    Added new script and resources

---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public GUIText scoreText;
+	public int score;
+
+	// Use this for initialization
+	 void Start () {
+		score = 0;
+		UpdateScore ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void AddScore (int newScoreValue)
+	{
+		score += newScoreValue;	
+		UpdateScore ();
+	}
+
+	void UpdateScore() 
+	{
+		scoreText.text = "Score: " + score;
+	}
+}
